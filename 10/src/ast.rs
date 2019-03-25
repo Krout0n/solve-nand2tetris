54d3b3a8c7e0161de30732cf63fa6fba9e10bf28
Knium_ <hi.im.knium@gmail.com>
2019-03-26 00:40:34 +0900

    Add KeyConstant.

--- 10/src/ast.rs
+++ 10/src/ast.rs
@@ -1,3 +1,5 @@
+use token::KeyWordKind;
+
 pub type Statements = Vec<Statement>;
 
 pub enum Statement {
@@ -19,19 +21,37 @@ pub enum Statement {
 }
 
 #[derive(Debug, PartialEq)]
+pub enum KeyConstant {
+    True,
+    False,
+    Null,
+    This,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Expr {
     BinOP(char, Box<Expr>, Box<Expr>),
     Unary(char, Box<Expr>),
     Integer(u16),
+    Keyword(KeyConstant),
 }
 
 impl Expr {
-
     pub fn binop(op: char, lhs: Self, rhs: Self) -> Self {
         Expr::BinOP(op, Box::new(lhs), Box::new(rhs))
     }
 
     pub fn unary(op: char, expr: Expr) -> Self {
         Expr::Unary(op, Box::new(expr))
+    }
+
+    pub fn keyword(kind: KeyWordKind) -> Self {
+        Expr::Keyword(match kind {
+            KeyWordKind::True => KeyConstant::True,
+            KeyWordKind::False => KeyConstant::False,
+            KeyWordKind::Null => KeyConstant::Null,
+            KeyWordKind::This => KeyConstant::This,
+            _ => panic!("Unexpected keyword! {:?}", kind),
+        })
     }
 }
--- 10/src/parser.rs
+++ 10/src/parser.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
 
-use ast::{Expr, Statements};
 use self::Expr::*;
-use token::*;
+use ast::{Expr, KeyConstant, Statements};
 use token::Token::*;
+use token::*;
 
 pub struct Parser {
     index: usize,
@@ -26,6 +26,7 @@ impl Parser {
             IntegerConstant(i) => Integer(i),
             Symbol('~') => Expr::unary('~', self.term()),
             Symbol('-') => Expr::unary('-', self.term()),
+            KeyWord(kind) => Expr::keyword(kind),
             _ => panic!("term error! {:?}", t),
         }
     }
@@ -68,14 +69,37 @@ mod tests {
     use lexer::Lexer;
     use token::*;
 
-    use ast::{Expr, Statement};
     use self::Expr::*;
+    use self::KeyConstant::*;
     use super::Parser;
+    use ast::{Expr, KeyConstant, Statement};
+
+    const I1: Expr = Integer(1);
 
     fn tokenize(s: &'static str) -> Vec<Token> {
         let mut l = Lexer::new(s.to_string());
         l.lex_all();
         l.result
+    }
+
+    #[test]
+    fn term() {
+        fn test(t: Vec<Token>, right: Expr) {
+            let mut p = Parser::new(t);
+            assert_eq!(p.term(), right);
+        }
+
+        test(tokenize("-1"), Expr::unary('-', Integer(1)));
+
+        test(tokenize("~true"), Expr::unary('~', Expr::Keyword(True)));
+
+        test(tokenize("true"), Expr::Keyword(True));
+
+        test(tokenize("false"), Expr::Keyword(False));
+
+        test(tokenize("this"), Expr::Keyword(This));
+
+        test(tokenize("null"), Expr::Keyword(Null));
     }
 
     #[test]
@@ -85,24 +109,11 @@ mod tests {
             assert_eq!(p.expr(), right);
         }
 
-        test(
-            tokenize("1+1"),
-            Expr::binop('+', Integer(1), Integer(1))
-        );
-
-        test(
-            tokenize("-1"),
-            Expr::unary('-', Integer(1))
-        );
-
-        test(
-            tokenize("~1"),
-            Expr::unary('~', Integer(1))
-        );
+        test(tokenize("1+1"), Expr::binop('+', I1, I1));
 
         test(
             tokenize("-1+1"),
-            Expr::binop('+', Expr::unary('-', Integer(1)), Integer(1))
+            Expr::binop('+', Expr::unary('-', Integer(1)), Integer(1)),
         );
     }
 }
