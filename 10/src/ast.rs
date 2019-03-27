a0210d5bf95c69239ff833ef9873a7db124f6254
Knium_ <hi.im.knium@gmail.com>
2019-03-27 22:58:11 +0900

    Add definition of Class and so on.

--- 10/src/ast.rs
+++ 10/src/ast.rs
@@ -1,7 +1,118 @@
+use self::Token::*;
 use token::KeyWordKind;
+use token::Token;
 
-type VarName = String;
-type SubroutineName = String;
+type Identifier = String;
+type ClassName = Identifier;
+type VarName = Identifier;
+type SubroutineName = Identifier;
+
+#[derive(Debug, PartialEq)]
+pub struct Class {
+    name: ClassName,
+    class_var_decs: Vec<ClassVarDec>,
+    subroutine_decs: Vec<SubroutineDec>,
+}
+
+impl Class {
+    pub fn new(name: ClassName, class_var_decs: Vec<ClassVarDec>, subroutine_decs: Vec<SubroutineDec>) -> Self {
+        Class {
+            name,
+            class_var_decs,
+            subroutine_decs,
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+pub enum StaticOrField {
+    Static,
+    Field,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum VarType {
+    Int,
+    Char,
+    Boolean,
+    Void,
+    Defined(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ClassVarDec {
+    static_or_field: StaticOrField,
+    var_type: VarType,
+    var_name: Vec<VarName>,
+}
+
+impl ClassVarDec {
+    pub fn new(static_or_field: StaticOrField, var_type: VarType, var_name: Vec<VarName>) -> Self {
+        ClassVarDec {
+            static_or_field,
+            var_type,
+            var_name,
+        }
+    }
+}
+
+// TODO Renameしましょう
+#[derive(Debug, PartialEq)]
+pub enum SubroutineKind {
+    Constructor,
+    Function,
+    Method,
+}
+
+impl SubroutineKind {
+    pub fn by_token(t: Token) -> Self {
+        match t {
+            KeyWord(KeyWordKind::Constructor) => SubroutineKind::Constructor,
+            KeyWord(KeyWordKind::Function) => SubroutineKind::Function,
+            KeyWord(KeyWordKind::Method) => SubroutineKind::Method,
+            _ => panic!(
+                "Expected one of 'constructor' | 'function' | 'method', but got {:?}'",
+                t
+            ),
+        }
+    }
+}
+
+pub type Parameter = (VarType, VarName);
+
+#[derive(Debug, PartialEq)]
+pub enum Subroutine {
+    VarDec(VarType, Vec<VarName>),
+    Stmts(Stmts),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SubroutineDec {
+    kind: SubroutineKind,
+    ret_type: VarType,
+    subroutine_name: SubroutineName,
+    parameter_list: Vec<Parameter>,
+    subroutine_body: Vec<Subroutine>,
+}
+
+impl SubroutineDec {
+    pub fn new(
+        kind: SubroutineKind,
+        ret_type: VarType,
+        subroutine_name: SubroutineName,
+        parameter_list: Vec<Parameter>,
+        subroutine_body: Vec<Subroutine>,
+    ) -> Self {
+        SubroutineDec {
+            kind,
+            ret_type,
+            subroutine_name,
+            parameter_list,
+            subroutine_body,
+        }
+    }
+}
 
 pub type Stmts = Vec<Stmt>;
 
